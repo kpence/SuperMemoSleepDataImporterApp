2b40447f61894187497b559ebc246c6e899a0cab
kpence <kpence@tamu.edu>
2021-07-13 20:22:48 -0500

    initial commit

---
+++ FitbitSuperMemoSleepDataImporterApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fitbit.Api.Portable;
+using SleepDataImporter;
+
+namespace $safeprojectname$
+{
+	public class Program
+    {
+        //var file = @"C:\Users\james\SuperMemo\sleep\sleep.tim";
+        //var sleepReg = new SleepDataRegistry(file);
+        //sleepReg.ReadSleepData();
+        //var credentials = new FitbitAppCredentials();
+        //credentials.ClientId = "";
+        //credentials.ClientSecret = "";
+        //FitbitClient client = new FitbitClient(credentials, );
+        // Signature of method which may be useful: public Task<SleepLogListBase> GetSleepLogListAsync(DateTime dateToList, SleepEnum decisionDate, SortEnum sort, int limit, string encodedUserId = null);
+    }
+}
