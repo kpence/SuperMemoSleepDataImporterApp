b0fcf06b77b290f7784f9434828e3604b78e1cf5
kpence <kpence@tamu.edu>
2021-07-22 20:25:41 -0500

    Copy sample console program from fitbitNET repo

--- FitbitSuperMemoSleepDataImporterApp/Program.cs
+++ FitbitSuperMemoSleepDataImporterApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 using Fitbit.Api.Portable;
+using Fitbit.Models;
 using SleepDataImporter;
 
 namespace FitbitSuperMemoSleepDataImporterApp
@@ -12,11 +13,26 @@ namespace FitbitSuperMemoSleepDataImporterApp
             //var file = @"C:\Users\james\SuperMemo\sleep\sleep.tim";
             //var sleepReg = new SleepDataRegistry(file);
             //sleepReg.ReadSleepData();
-            //var credentials = new FitbitAppCredentials();
-            //credentials.ClientId = "";
-            //credentials.ClientSecret = "";
-            //FitbitClient client = new FitbitClient(credentials, 
+            /*
+            var credentials = new FitbitAppCredentials();
+            credentials.ClientId = "";
+            credentials.ClientSecret = "";
+            FitbitClient client = new FitbitClient(credentials, null);
+            */
             // Signature of method which may be useful: public Task<SleepLogListBase> GetSleepLogListAsync(DateTime dateToList, SleepEnum decisionDate, SortEnum sort, int limit, string encodedUserId = null);
+
+            // Authorize with FitBit
+            FitbitClient fc = AuthorizationHelper.GetAuthorizedFitBitClient("activity ", "nutrition ", "heartrate ", "location ", "nutrition ", "profile ", "settings ", "sleep ", "social ", "weight");
+
+            // Retrieve the weight information for today
+            DateTime start = DateTime.Now;
+            Console.Write("Processing {0}...", start.ToShortDateString());
+            var weight = fc.GetWeightAsync(start, DateRangePeriod.OneMonth).Result;
+            Console.WriteLine("found {0} entries.", weight.Weights.Count);
+
+            // Save the downloaded information to disk
+            System.Xml.Serialization.XmlSerializer src = new System.Xml.Serialization.XmlSerializer(typeof(List<Weight>));
+
         }
     }
 }
