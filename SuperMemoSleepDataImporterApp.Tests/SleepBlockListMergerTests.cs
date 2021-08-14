29c07e9695054e49d6b90f943c9ebc1827db2d6d
kpence <kpence@tamu.edu>
2021-08-13 20:30:27 -0500

    Slightly modify list merger test implementation (still not finished yet)

--- SuperMemoSleepDataImporterApp.Tests/SleepBlockListMergerTests.cs
+++ SuperMemoSleepDataImporterApp.Tests/SleepBlockListMergerTests.cs
@@ -20,119 +20,167 @@ namespace SuperMemoSleepDataImporterApp.Tests
             }
         };
 
-        private readonly Dictionary<string, List<ISleepBlockAdapter>> SleepBlockList = new Dictionary<string, List<ISleepBlockAdapter>>
+        private readonly Dictionary<string, Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>> SleepBlockTestList =
+            new Dictionary<string, Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>>
         {
             {
                 "Empty New List",
-                new List<ISleepBlockAdapter> { }
+                new Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>
+                (
+                    new List<ISleepBlockAdapter> { },
+                    new List<ISleepBlockAdapter> { }
+                )
             },
             {
                 "Sorted, No Overlap, All After",
-                new List<ISleepBlockAdapter>
-                {
-                    new SleepBlockAdapter
+                new Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>
+                (
+                    new List<ISleepBlockAdapter>
                     {
-                        Start = new DateTime(2021, 1, 3, 1, 0, 0),
-                        End = new DateTime(2021, 1, 3, 8, 0, 0)
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 3, 1, 0, 0),
+                            End = new DateTime(2021, 1, 3, 8, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 4, 1, 0, 0),
+                            End = new DateTime(2021, 1, 4, 8, 0, 0)
+                        }
                     },
-                    new SleepBlockAdapter
+                    new List<ISleepBlockAdapter>
                     {
-                        Start = new DateTime(2021, 1, 4, 1, 0, 0),
-                        End = new DateTime(2021, 1, 4, 8, 0, 0)
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 3, 1, 0, 0),
+                            End = new DateTime(2021, 1, 3, 8, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 4, 1, 0, 0),
+                            End = new DateTime(2021, 1, 4, 8, 0, 0)
+                        }
                     }
-                }
+                )
             },
             {
                 "Sorted, No Overlap, All Before",
-                new List<ISleepBlockAdapter>
-                {
-                    new SleepBlockAdapter
+                new Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>
+                (
+                    new List<ISleepBlockAdapter>
                     {
-                        Start = new DateTime(2021, 1, 1, 1, 0, 0),
-                        End = new DateTime(2021, 1, 1, 2, 0, 0)
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 1, 1, 0, 0),
+                            End = new DateTime(2021, 1, 1, 2, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 4, 2, 0, 0),
+                            End = new DateTime(2021, 1, 4, 3, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 4, 3, 0, 0),
+                            End = new DateTime(2021, 1, 4, 4, 0, 0)
+                        }
                     },
-                    new SleepBlockAdapter
+                    new List<ISleepBlockAdapter>
                     {
-                        Start = new DateTime(2021, 1, 4, 2, 0, 0),
-                        End = new DateTime(2021, 1, 4, 3, 0, 0)
-                    },
-                    new SleepBlockAdapter
-                    {
-                        Start = new DateTime(2021, 1, 4, 3, 0, 0),
-                        End = new DateTime(2021, 1, 4, 4, 0, 0)
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 1, 1, 0, 0),
+                            End = new DateTime(2021, 1, 1, 2, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 4, 2, 0, 0),
+                            End = new DateTime(2021, 1, 4, 3, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 4, 3, 0, 0),
+                            End = new DateTime(2021, 1, 4, 4, 0, 0)
+                        }
                     }
-                }
+                )
             },
             {
                 "Unsorted - No Convergence",
-                new List<ISleepBlockAdapter>
-                {
-                }
+                new Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>
+                (
+                    new List<ISleepBlockAdapter>
+                    {
+                    },
+                    new List<ISleepBlockAdapter>
+                    {
+                    }
+                )
+            },
+            {
+                "Overlap1",
+                new Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>
+                (
+                    new List<ISleepBlockAdapter>
+                    {
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 1, 3, 0, 0),
+                            End = new DateTime(2021, 1, 1, 9, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 2, 1, 0, 0),
+                            End = new DateTime(2021, 1, 2, 7, 0, 0)
+                        }
+                    },
+                    new List<ISleepBlockAdapter>
+                    {
+                    }
+                )
             },
             {
                 "Partial Overlap",
-                new List<ISleepBlockAdapter>
-                {
-                }
+                new Tuple<List<ISleepBlockAdapter>, List<ISleepBlockAdapter>>
+                (
+                    new List<ISleepBlockAdapter>
+                    {
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 1, 1, 4, 0, 0),
+                            End = new DateTime(2021, 1, 1, 8, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2020, 1, 2, 4, 0, 0),
+                            End = new DateTime(2021, 1, 3, 8, 0, 0)
+                        },
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 3, 2, 4, 0, 0),
+                            End = new DateTime(2021, 3, 2, 8, 0, 0)
+                        }
+                    },
+                    new List<ISleepBlockAdapter>
+                    {
+                        new SleepBlockAdapter
+                        {
+                            Start = new DateTime(2021, 3, 2, 4, 0, 0),
+                            End = new DateTime(2021, 3, 2, 8, 0, 0)
+                        }
+                    }
+                )
             }
         };
 
-        private readonly Dictionary<string, List<ISleepBlockAdapter>> ExpectedSleepBlockListResults = new Dictionary<string, List<ISleepBlockAdapter>>
+        public static IEnumerable<object[]> TestKeys => new List<object[]>
         {
-            {
-                "Empty New List",
-                new List<ISleepBlockAdapter> {
-                }
-            },
-            {
-                "Sorted, No Overlap, All After",
-                new List<ISleepBlockAdapter>
-                {
-                    new SleepBlockAdapter
-                    {
-                        Start = new DateTime(2021, 1, 3, 1, 0, 0),
-                        End = new DateTime(2021, 1, 3, 8, 0, 0)
-                    },
-                    new SleepBlockAdapter
-                    {
-                        Start = new DateTime(2021, 1, 4, 1, 0, 0),
-                        End = new DateTime(2021, 1, 4, 8, 0, 0)
-                    }
-                }
-            },
-            {
-                "Sorted, No Overlap, All Before",
-                new List<ISleepBlockAdapter>
-                {
-                    new SleepBlockAdapter
-                    {
-                        Start = new DateTime(2021, 1, 1, 1, 0, 0),
-                        End = new DateTime(2021, 1, 1, 2, 0, 0)
-                    },
-                    new SleepBlockAdapter
-                    {
-                        Start = new DateTime(2021, 1, 4, 2, 0, 0),
-                        End = new DateTime(2021, 1, 4, 3, 0, 0)
-                    },
-                    new SleepBlockAdapter
-                    {
-                        Start = new DateTime(2021, 1, 4, 3, 0, 0),
-                        End = new DateTime(2021, 1, 4, 4, 0, 0)
-                    }
-                }
-            },
-            {
-                "Unsorted - No Convergence",
-                new List<ISleepBlockAdapter>
-                {
-                }
-            },
-            {
-                "Partial Overlap",
-                new List<ISleepBlockAdapter>
-                {
-                }
-            }
+            new object[] { "Empty New List" },
+            new object[] { "Sorted, No Overlap, All After" },
+            new object[] { "Sorted, No Overlap, All Before" },
+            new object[] { "Unsorted - No Convergence" },
+            new object[] { "Partial Overlap" },
+            new object[] { "Overlap1" },
         };
 
         private void CompareSleepBlockLists(List<ISleepBlockAdapter> sleepBlockList1, List<ISleepBlockAdapter> sleepBlockList2)
@@ -150,11 +198,11 @@ namespace SuperMemoSleepDataImporterApp.Tests
         {
             var Results = new List<ISleepBlockAdapter>(
                 SleepBlockListMerger.SubtractExistingFromList(
-                    SleepBlockList[TestKey].ToArray(),
+                    SleepBlockTestList[TestKey].Item1.ToArray(),
                     ExistingSleepBlocks.ToArray()
                 )
             );
-            CompareSleepBlockLists(Results, ExpectedSleepBlockListResults[TestKey]);
+            CompareSleepBlockLists(Results, SleepBlockTestList[TestKey].Item2);
         }
 
         [Fact]
@@ -215,17 +263,11 @@ namespace SuperMemoSleepDataImporterApp.Tests
             Assert.False(SleepBlockAdapter.IsOverlap(sb, sb_noOverlap2));
         }
 
-        [Fact]
-        public void TestEmptyNewList()
+        [Theory]
+        [MemberData(nameof(TestKeys))]
+        public void TestSortedNoOverlap(string key)
         {
-            TestSleepBlockList("Empty New List");
-        }
-
-        [Fact]
-        public void TestSortedNoOverlap()
-        {
-            TestSleepBlockList("Sorted, No Overlap, All After");
-            TestSleepBlockList("Sorted, No Overlap, All Before");
+            TestSleepBlockList(key);
         }
     }
 }
